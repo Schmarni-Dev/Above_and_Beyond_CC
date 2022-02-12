49b7d53cecf8080ee2697fb7bff275bf084f7d87
Schmarni <marnistromer@gmail.com>
2022-02-12 19:45:04 +0100

    Now?

--- updater.lua
+++ updater.lua
@@ -1,10 +1,15 @@
-print("CUM")
+local tArgs = { ... }
 local inputs = {s ={
     repo = "Above_and_Beyond_CC",
     user = "Schmarni-Dev",
     file = "updater.lua",
     branch = "main"
 }}
+
+if tArgs == nil then
+    print("error please supply args")
+    return
+end
 
 print(textutils.serialise(inputs["s"]))
 fs.delete("updater.lua")
