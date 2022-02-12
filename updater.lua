75561145115e1023b477bff46c135410ea884ebb
Schmarni <marnistromer@gmail.com>
2022-02-12 20:12:41 +0100

    now? pls

--- updater.lua
+++ updater.lua
@@ -1,4 +1,5 @@
 local tArgs = { ... }
+local Vinputs = {}
 local inputs = {s ={
     repo = "Above_and_Beyond_CC",
     user = "Schmarni-Dev",
@@ -6,11 +7,21 @@ local inputs = {s ={
     branch = "main"
 }}
 
-if tArgs == nil then
-    print("error please supply args")
-    return
+local function isValid(input)
+    for k, v in pairs(t) do
+        print(v)
+    end
 end
-print(tArgs)
-print(textutils.serialise(inputs["s"]))
-fs.delete("updater.lua")
-print("Done")
+
+local function main(value)
+    print(textutils.serialise(inputs["s"]))
+    fs.delete("updater.lua")
+    print("Done")
+end
+
+print("Error: please supply valid args")
+
+if tArgs[1] == "S" then
+    main("s")
+end
+    
