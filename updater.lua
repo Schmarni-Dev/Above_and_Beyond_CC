bbe7272fe76dfcd24ebe7498254e12a0e0a4e4ca
Schmarni <marnistromer@gmail.com>
2022-02-13 03:36:56 +0100

    Updated updater.lua

--- updater.lua
+++ updater.lua
@@ -1,12 +1,20 @@
 local git = require("github")
-local tArgs = { ... }
-local Vinputs = {"a","b","x","y"}
-local inputs = {_self ={
-    repo = "Above_and_Beyond_CC",
-    user = "Schmarni-Dev",
-    file = "updater.lua",
-    branch = "main"
-}}
+local tArgs = {...}
+local Vinputs = {"hello"}
+local inputs = {
+    hello = {
+        repo = "Above_and_Beyond_CC",
+        user = "Schmarni-Dev",
+        file = "printHelloWorld.lua",
+        branch = "main"
+    },
+    _self = {
+        repo = "Above_and_Beyond_CC",
+        user = "Schmarni-Dev",
+        file = "updater.lua",
+        branch = "main"
+    }
+}
 
 local function isValid(input)
     for k, v in pairs(Vinputs) do
@@ -21,27 +29,24 @@ local function Update(value)
     local t = inputs[value]
     local fPath = shell.resolve(t["file"])
     fs.delete(fPath)
-    git.getFile(t["file"],t["branch"],t["user"],t["repo"])
+    git.getFile(t["file"], t["branch"], t["user"], t["repo"])
     print("Done")
     shell.run(t["file"])
 end
 
-local function SelfUpdate(value,args)
+local function SelfUpdate(value, args)
     local t = inputs[value]
     local fPath = shell.resolve(t["file"])
     fs.delete(fPath)
-    git.getFile(t["file"],t["branch"],t["user"],t["repo"])
+    git.getFile(t["file"], t["branch"], t["user"], t["repo"])
     print("Done")
-    shell.run(t["file "..args.." self"])
+    shell.run(t["file " .. args .. " self"])
 end
 
-
-
 if tArgs[2] ~= "self" then
-    SelfUpdate("_self",tArgs[1])
+    SelfUpdate("_self", tArgs[1])
 elseif isValid(tArgs[1]) then
     Update(tArgs[1])
 else
     print("Error: please supply valid args")
 end
-    
