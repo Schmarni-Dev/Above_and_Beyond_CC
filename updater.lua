1e863f183bd40588b1739659f48c72062a695d7a
Schmarni <marnistromer@gmail.com>
2022-02-13 03:34:17 +0100

    updated updater and added print hello world file

--- updater.lua
+++ updater.lua
@@ -1,6 +1,7 @@
+local git = require("github")
 local tArgs = { ... }
 local Vinputs = {"a","b","x","y"}
-local inputs = {s ={
+local inputs = {_self ={
     repo = "Above_and_Beyond_CC",
     user = "Schmarni-Dev",
     file = "updater.lua",
@@ -9,20 +10,38 @@ local inputs = {s ={
 
 local function isValid(input)
     for k, v in pairs(Vinputs) do
-        print(v)
+        if v == input then
+            return true
+        end
     end
+    return false
 end
 
-local function main(value)
-    print(textutils.serialise(inputs["s"]))
-    fs.delete("updater.lua")
+local function Update(value)
+    local t = inputs[value]
+    local fPath = shell.resolve(t["file"])
+    fs.delete(fPath)
+    git.getFile(t["file"],t["branch"],t["user"],t["repo"])
     print("Done")
+    shell.run(t["file"])
 end
 
-print("Error: please supply valid args")
-isValid("CUM")
+local function SelfUpdate(value,args)
+    local t = inputs[value]
+    local fPath = shell.resolve(t["file"])
+    fs.delete(fPath)
+    git.getFile(t["file"],t["branch"],t["user"],t["repo"])
+    print("Done")
+    shell.run(t["file "..args.." self"])
+end
 
-if tArgs[1] == "S" then
-    main("s")
+
+
+if tArgs[2] ~= "self" then
+    SelfUpdate("_self",tArgs[1])
+elseif isValid(tArgs[1]) then
+    Update(tArgs[1])
+else
+    print("Error: please supply valid args")
 end
     
